073adf027467d492a31e6d74b80348f314462906
Vassily <amilkar.f.p@gmail.com>
2025-07-07 20:02:11 -0500

    Agregar componente Actividades y simplificar App.jsx

---
+++ src/assets/components/Actividades.jsx
@@ -0,0 +1,12 @@
+export const Actividades = () => {
+  return (
+    <>
+      <h1>List TO-DO</h1>
+      <button>ingresar tarea</button>
+      <h2>titulo de la tarea </h2>
+      <textarea name="" id="" placeholder="Define la tarea aqui"></textarea>
+      <h3>fecha limite</h3>
+      <span>fecha limite</span>
+    </>
+  );
+};
